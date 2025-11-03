0d00bc3f74bc49d534fb441094b025a05b54f3de
Dtar380 <dtar380@icloud.com>
2025-11-03 16:02:32 +0100

    Added Menu class to Builder
    This Menu class will contain all the iquirer prompts to get the information needed to build or update the docker-compose or the env files

--- src/cli/builder.py
+++ src/cli/builder.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import inspect
+from typing import Any
 
 from InquirerPy import inquirer  # type: ignore
 from InquirerPy.validator import EmptyInputValidator  # type: ignore
@@ -10,10 +11,46 @@ from ..utils.cli import clear  # type: ignore
 from .custom_group import CustomGroup
 
 
+class Menus:
+
+    # Construct service contents for docker-compose
+    def service(self) -> dict[str, Any]:
+        return {}
+
+    def __get_name(self) -> str:
+        return ""
+
+    def __get_ports(self) -> str:
+        return ""
+
+    def __expose(self) -> bool:
+        return True
+
+    def __resources(self) -> dict[str, int]:
+        return {}
+
+    # Construct env file contents
+    def env(self) -> dict[str, Any]:
+        return {}
+
+    def __get_jar(self) -> str:
+        return ""
+
+    def __use_args(self) -> str:
+        return ""
+
+    def __get_heaps(self) -> dict[str, str]:
+        return {}
+
+    def __get_type(self) -> str:
+        return ""
+
+
 class Builder(CustomGroup):
 
     def __init__(self) -> None:
         super().__init__()
+        self.menus = Menus()
 
     def create(self) -> Command:
 
