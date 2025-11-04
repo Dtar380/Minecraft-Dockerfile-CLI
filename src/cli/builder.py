157f32dfd4c790d51cfb3b0bbb8ed695f2c2eef4
Dtar380 <dtar380@icloud.com>
2025-11-04 16:38:02 +0100

    Added callback for create method
    Added options for create method

--- src/cli/builder.py
+++ src/cli/builder.py
@@ -14,7 +14,10 @@ from .custom_group import CustomGroup
 class Menus:
 
     def __init__(
-        self, network: str | None = None, update: bool = False
+        self,
+        network: str | None = None,
+        update: bool = False,
+        defaults: dict[str, Any] | None = None,
     ) -> None:
         self.network = network
         self.update = update
@@ -33,7 +36,9 @@ class Menus:
         return {}
 
     # Construct env file contents
-    def env(self) -> dict[str, Any]:
+    def env(
+        self, name: str, resources: dict[str, Any] | None = None
+    ) -> dict[str, Any]:
         return {}
 
     def __get_jar(self) -> str:
@@ -45,9 +50,6 @@ class Menus:
     def __get_heaps(self) -> dict[str, str]:
         return {}
 
-    def __get_type(self) -> str:
-        return ""
-
 
 class Builder(CustomGroup):
 
@@ -57,10 +59,55 @@ class Builder(CustomGroup):
     def create(self) -> Command:
 
         help = ""
-        options = [Option()]
+        options = [Option(["--network"], is_flag=True, default=False)]
 
-        def callback() -> dict[str, Any]:
-            return {}
+        def callback(network: bool = False) -> None:
+            services: set[dict[str, Any]] = set()
+            networks: set[str] = set()
+            envs: set[dict[str, Any]] = set()
+
+            if not network:
+                menu = Menus()
+
+                name = inquirer.text(
+                    message="Enter the name of the service: "
+                ).execute()
+
+                service = menu.service(name=name)
+
+                resources = service.get("resources") or None
+                env = menu.env(name=name, resources=resources)
+
+                services.add(service)
+                envs.add(env)
+
+                return None
+
+            network_name = inquirer.text(
+                message="Enter the name of the network: "
+            ).execute()
+            networks.add(network_name)
+
+            menu = Menus(network=network_name)
+
+            while True:
+                name = inquirer.text(
+                    message="Enter the name of the service: "
+                ).execute()
+
+                service = menu.service(name=name)
+
+                resources = service.get("resources") or None
+                env = menu.env(name=name, resources=resources)
+
+                services.add(service)
+                envs.add(env)
+
+                if not inquirer.confirm(
+                    message=f"Want to continue adding services? (Count: {len(services)})",
+                    default=True if len(services) < 2 else False,
+                ).execute():
+                    break
 
         return Command(
             name=inspect.currentframe().f_code.co_name,  # type: ignore
@@ -74,8 +121,8 @@ class Builder(CustomGroup):
         help = ""
         options = [Option()]
 
-        def callback() -> dict[str, Any]:
-            return {}
+        def callback() -> None:
+            pass
 
         return Command(
             name=inspect.currentframe().f_code.co_name,  # type: ignore
