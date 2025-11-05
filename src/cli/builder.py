b768a9ba4fd8f30cf525199bf481a6131f594134
Dtar380 <dtar380@icloud.com>
2025-11-05 19:34:10 +0100

    Added Resources menu
    Added psUtils to check for CPU count and RAM ammount of the system

--- src/cli/builder.py
+++ src/cli/builder.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
 import inspect
-from pathlib import Path, PurePath
+from pathlib import Path
 from typing import Any
 
-from InquirerPy import inquirer
-from InquirerPy.validator import EmptyInputValidator
+from InquirerPy import inquirer  # type: ignore
+from InquirerPy.validator import EmptyInputValidator  # type: ignore
 from click import Command, Option
+import psutil  # type: ignore
 
 from ..core.manage_json import read_json, write_json  # type: ignore
 from ..core.manage_templates import template_to_file
@@ -21,17 +22,28 @@ class Menus:
     ) -> None:
         self.network = network
         self.update = update
+
+        self.cpus: float = psutil.cpu_count(logical=True) or 0
+        self.memory: float = (
+            psutil.virtual_memory().available // 1024**2 - 512 or 0
+        )
+
         self.ports: dict[str, int] = {}
-        self.resources = dict[str, Any]
+        self.resources: dict[str, int] = {}
+
+        if self.memory < 512:
+            print("WARNING: RAM AMOUNT TOO LOW")
+        clear(2)
 
     # Construct service contents for docker-compose
     def service(self, name: str) -> dict[str, Any]:
         self.__get_ports()
-        ports = [f"${{{port}}}:${{{port}}}" for port, _ in self.ports.items()]
         expose = self.__expose()
+        not_exposed = set([port for port in self.ports if port not in expose])
+        ports = set([f"${{{port}}}:${{{port}}}" for port in not_exposed])
         resources = self.__resources()
 
-        service = {
+        service: dict[str, Any] = {
             "name": name,
             "build": {"context": f"./servers/{name}/"},
             "env_file": f"./servers/{name}/.env",
@@ -68,29 +80,77 @@ class Menus:
 
             if inquirer.confirm(
                 message=f"Want to add {port_name} assigned to port {port}? ",
-                default=True,
+                default=False,
             ).execute():
                 self.ports[port_name] = port
 
-            if inquirer.confirm(
-                message="Want to add more ports? ", default=False
-            ).execute():
-                break
+                if inquirer.confirm(
+                    message="Want to add more ports? ", default=False
+                ).execute():
+                    break
 
-    def __expose(self) -> set[int]:
-        expose: set[int] = set()
+    def __expose(self) -> set[str]:
+        expose: set[str] = set()
 
         for name, port in self.ports.items():
+            clear(1)
+
             if inquirer.confirm(
                 message=f"Want to expose {name} assigned to {port}? ",
                 default=False,
             ).execute():
-                expose.add(port)
+                expose.add(f"${{{name}}}")
 
         return expose
 
     def __resources(self) -> dict[str, Any]:
-        return {}
+        while True:
+            cpus_limit: float = inquirer.number(
+                message="Select a limit of CPUs for this service: ",
+                min_allowed=0,
+                max_allowed=self.cpus,
+                float_allowed=True,
+                validate=EmptyInputValidator(),
+            ).execute()
+            cpus_reservation: float = inquirer.number(
+                message="Select a CPUs allocation for this service: ",
+                min_allowed=0,
+                max_allowed=cpus_limit,
+                float_allowed=True,
+                validate=EmptyInputValidator(),
+            ).execute()
+
+            memory_limit: float = inquirer.number(
+                message="Select a limit of RAM for this service (in MB): ",
+                min_allowed=0,
+                max_allowed=self.memory,
+                float_allowed=True,
+                validate=EmptyInputValidator(),
+            ).execute()
+            memory_reservation: float = inquirer.number(
+                message="Select a RAM allocation for this service (in MB): ",
+                min_allowed=0,
+                max_allowed=memory_limit,
+                float_allowed=True,
+                validate=EmptyInputValidator(),
+            ).execute()
+
+            if inquirer.confirm(
+                message="Confirm the RAM and CPU allocation for this service",
+                default=False,
+            ).execute():
+                break
+
+        self.cpus -= cpus_limit
+        self.memory -= memory_limit
+
+        return {
+            "limits": {"cpus": cpus_limit, "memory": memory_limit},
+            "reservations": {
+                "cpus": cpus_reservation,
+                "memory": memory_reservation,
+            },
+        }
 
     # Construct env file contents
     def env(self, name: str) -> dict[str, Any]:
@@ -112,12 +172,10 @@ class Builder(CustomGroup):
         super().__init__()
 
     def create(self) -> Command:
-
         help = ""
         options = [Option(["--network"], is_flag=True, default=False)]
 
         def callback(network: bool = False) -> None:
-
             services: set[dict[str, Any]] = set([])
             networks: set[str] = set([])
             envs: set[dict[str, Any]] = set([])
@@ -145,9 +203,11 @@ class Builder(CustomGroup):
                     services.add(service)
                     envs.add(env)
 
+                    clear(1)
+
                     if not inquirer.confirm(
                         message=f"Want to continue adding services? (Count: {len(services)})",
-                        default=True if len(services) < 2 else False,
+                        default=False,
                     ).execute():
                         break
 
@@ -169,7 +229,6 @@ class Builder(CustomGroup):
         )
 
     def update(self) -> Command:
-
         help = ""
         options = [Option()]
 
@@ -186,6 +245,7 @@ class Builder(CustomGroup):
     def __get_data(
         self, menu: Menus, get_service: bool = True, get_env: bool = True
     ) -> tuple[dict[str, Any], dict[str, Any]]:
+        clear(1)
 
         name = self.__get_name(message="Enter the name of the service: ")
 
@@ -200,11 +260,18 @@ class Builder(CustomGroup):
         return (service, env)
 
     def __get_name(self, message: str) -> str:
-        return str(
-            inquirer.text(
+        while True:
+            name = inquirer.text(
                 message=message, validate=EmptyInputValidator()
             ).execute()
-        )
+
+            if inquirer.confirm(
+                message=f"Want to name this service {name}? ",
+                default=False,
+            ).execute():
+                break
+
+        return str(name)
 
     def __save_files(self, data: dict[str, Any]) -> None:
         write_json(Path(), data)
